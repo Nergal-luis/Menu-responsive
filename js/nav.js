36a099e10888c56c4f247d85297561f9883fa051
Nergal-luis <leonkennedyevil2@gmail.com>
2020-08-25 12:31:24 -0600

    menu
    menu responsive

---
+++ js/nav.js
@@ -0,0 +1,15 @@
+var botonlis4 = document.getElementById('lisboton4');
+var boton4 = document.getElementById('boton4');
+
+
+function desplegarboton4() {
+  botonlis4.classList.add('mostrar');
+
+};
+
+function ocultarboton4() {
+  botonlis4.classList.remove('mostrar');
+
+};
+
+boton4.addEventListener("mouseover", desplegarboton4,true);
